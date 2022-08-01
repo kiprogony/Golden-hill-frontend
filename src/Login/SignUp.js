5782dd352dd0bec0143a91456a5581a867680de4
Daryl Rogony <darylrogony@gmail.com>
2022-08-01 09:50:29 +0300

    Fit: a signup function

---
+++ src/Login/SignUp.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+
+
+
+const theme = createTheme();
+
+export default function Signup() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    
+  )
+}
