afadbe6db890f002b7397e47bf6d893a1c85609d
Daryl Rogony <darylrogony@gmail.com>
2022-08-01 12:13:49 +0300

    Fit: a login function

--- src/Login/LogIn.js
+++ src/Login/LogIn.js
@@ -14,3 +14,14 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme();
+
+export default function SignInSide() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
